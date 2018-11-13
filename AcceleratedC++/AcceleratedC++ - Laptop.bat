6aa2588d6d09af33c068db8b8cf7bda52ef62794
AnthonyDas <anthony.das@gmail.com>
2018-11-13 12:17:20 +0000

    Renaming MS Visual Studio Project: "AcceleratedC++" to "AcceleratedCpp". Adding manual notes.

--- AcceleratedC++/AcceleratedC++ - Laptop.bat
+++
@@ -1,8 +0,0 @@
-C:
-
-cd C:\Users\AnthonyDas\Documents\Visual Studio 2017\Projects\AcceleratedC++\AcceleratedC++
-
-g++ -Wall -Wconversion -std=c++11 -mtune=native -O3 main1.cpp -o AcceleratedC++.exe
-
-
-pause
---
+++ AcceleratedCpp/AcceleratedCpp - Laptop.bat
@@ -0,0 +1,8 @@
+C:
+
+cd C:\Users\AnthonyDas\Documents\GitHub\AcceleratedCpp\AcceleratedCpp
+
+g++ -Wall -Wconversion -std=c++11 -mtune=native -O3 main1.cpp -o AcceleratedCpp.exe
+
+
+pause
